07466f47134ee28be18dd2beab579d785523caa8
Dong Wang <dongw2019@gmail.com>
2026-02-07 10:52:55 -0800

    Revert "fix func call"
    This reverts commit 021cf6b15486676068db53e474c288838efe19ce.

--- examples/diffusion_router/demo.py
+++ examples/diffusion_router/demo.py
@@ -46,8 +46,7 @@ def main():
 
     # Pre-register any workers specified on the command line
     for url in args.worker_urls:
-        router.worker_request_counts[url] = 0
-        router.worker_failure_counts[url] = 0
+        router.add_worker_sync(url)
         if args.verbose:
             print(f"[demo] Pre-registered worker: {url}")
 
